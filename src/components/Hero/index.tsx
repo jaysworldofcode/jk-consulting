834e72cddbb3a0ff71698d532dd0d7a5048391d5
Jay Lord Abueva <jaylord.abueva98@gmail.com>
2024-05-15 17:47:19 +0800

    fix: font changes and redirect links

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
                 className="hero-content wow fadeInUp mx-auto max-w-[780px] text-center"
                 data-wow-delay=".2s"
               >
-                <h1 className="mb-6 text-3xl font-bold leading-snug text-white sm:text-4xl sm:leading-snug lg:text-5xl lg:leading-[1.2]">
+                <h1 className="mb-6 text-3xl font-black leading-snug text-white sm:text-4xl sm:leading-snug lg:text-5xl lg:leading-[1.2]">
                   J.K. Consulting Agency
                 </h1>
                 <p className="mx-auto mb-9 max-w-[600px] text-base font-medium text-white sm:text-lg sm:leading-[1.44]">
@@ -24,7 +24,7 @@ const Hero = () => {
                 <ul className="mb-10 flex flex-wrap items-center justify-center gap-5">
                   <li>
                     <Link
-                      href="https://nextjstemplates.com/templates/play"
+                      href="/contact"
                       className="font-black inline-flex items-center justify-center rounded-md bg-white px-7 py-[14px] text-center text-base text-dark shadow-1 transition duration-300 ease-in-out hover:bg-gray-2"
                     >
                       Let's work together
